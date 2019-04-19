3dff1ab3d1b0b2de49278f430966d526720626a4
Christopher Doyle <christophercormac@outlook.com>
2019-04-19 16:57:00 +0100

    Integer representation

--- basic_interpreter/basic.py
+++ basic_interpreter/basic.py
@@ -1,5 +1,21 @@
+from enum import Enum
+
+
+class TokenType(Enum):
+    INTEGER = 1
+
 
 class Token:
 
     def __init__(self, type_, value):
-        pass
+        self.type_ = type_
+        self.value = value
+
+    def __str__(self):
+        if self.value is None:
+            return self.type_.name
+        else:
+            return f'{self.type_.name}:{self.value}'
+
+    def __repr__(self):
+        return self.__str__()
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,12 +1,14 @@
 import pytest
 
-from basic_interpreter.basic import Token
+from basic_interpreter.basic import Token, TokenType
 
 
-@pytest.mark.parameterized(
+@pytest.mark.parametrize(
     'type_, value, representation',
-    []
+    [
+        (TokenType.INTEGER, 5, 'INTEGER:5'),
+        (TokenType.INTEGER, None, 'INTEGER'),
+    ]
 )
 def test_token_representation(type_, value, representation):
     assert str(Token(type_, value)) == representation
-
